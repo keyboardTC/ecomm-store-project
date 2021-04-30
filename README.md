eb96641e90784195bc0f88b01d4ae2931805a247
keyboardTC <37123270+keyboardTC@users.noreply.github.com>
2021-04-30 10:12:29 -0400

    Products been generated from javascript Array

--- README.md
+++ README.md
@@ -16,3 +16,8 @@ Read and complete the steps in the following two documents:
 - The live url must be added to appropriate field of the repository's "About" (top-right)
 - Both urls (code, and live) will be collected prior to the first deliverable being due
 - See the [project overview](https://www.notion.so/juneate/Project-Webstore-28443f753b344d60b0176a24e14f17c9) for details on submissions, as well as a form
+
+![Lighthouse report](img/home-lighthouse.jpg)
+![Lighthouse report](img/homepage-mobile.jpg)
+![Lighthouse report](img/product-page-desktop.jpg)
+![Lighthouse report](img/product-page-mobile.jpg)
--- js/script.js
+++ js/script.js
@@ -10,3 +10,73 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+//========================== Shop page=================== //
+
+const products = [
+  {
+    prod_img: '1b.jpg',
+    prod_link: 'product-page.html',
+    prod_name: 'Product1',
+    prod_desc: 'Here is a shot of this product that might entice a user to click and add it to their cart.',
+    prod_prices: [50.00, 39.00],
+    prod_rating: 4.4,
+    prod_colour: ['Red', 'White'],
+    prod_weight: [100, 200]
+  }
+
+]
+
+// Select the element all products
+const products_table = document.querySelector('#prod');
+
+// Modify the new element, create the templete for all product.
+products.forEach((prod) => {
+
+  // Create article element to hold each product
+  const article_item = document.createElement('article')
+
+  // Adding class name product to the article created
+  article_item.classList.add('product') 
+
+  article_item.innerHTML = `
+  <img src="img/${prod.prod_img}" alt="${prod.prod_name}">
+  <div class="prod-details">
+      <h3><a href="${prod.prod_link}">${prod.prod_name}</a></h3>
+      <p>${prod.prod_desc} <a href="${prod.prod_link}">see more</a></p>
+      <dl class="rating">
+        <data value="${prod.prod_prices[1]}"><del>$${prod.prod_prices[0]}</del> <ins>$${prod.prod_prices[1]}</ins></data>
+        <dd><p>Rating : ${prod.prod_rating}</p> <span class="material-icons">star</span><span class="material-icons">star</span><span class="material-icons">star</span><span class="material-icons">star</span><span class="material-icons">star_half</span></dd>
+      </dl>
+  
+      <form class="iterm-properties">
+        <fieldset>
+          <legend>Colours</legend>
+          <ul>
+            <li><label><input type="radio" name="colour" value="r"> ${prod.prod_colour[0]}</label></li>
+            <li><label><input type="radio" name="colour" value="r"> ${prod.prod_colour[1]}</label></li>
+          </ul>
+        </fieldset>
+        <fieldset>
+          <legend>Weight</legend>
+          <ol>
+            <li><label><input type="radio" name="size" value="m">${prod.prod_weight[0]}</label></li>
+            <li><label><input type="radio" name="size" value="m">${prod.prod_weight[1]}</label></li>
+          </ol>
+        </fieldset>
+      </form>
+      <div class="prodcut-list-cart">
+        <button type="button" class="add-to-cart-btn"><i class="fa fas fa-shopping-cart"></i> Add to Cart</button>
+        <button type="button" class="add-to-fav"><i class="fa far fa-heart "></i></button>
+      </div>
+    </div>
+  `
+  // Adding the article element to the result section element
+  products_table.appendChild(article_item);
+
+  console.log(prod.prod_img)
+
+  
+})
+
+
